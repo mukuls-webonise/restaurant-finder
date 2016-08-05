0b06de123dad110f0929ef73baa236297881c7b4
Mukul Sharma <mukul.sharma@weboniselab.com>
2016-08-05 19:32:34 +0530

    Initial Commit

--- src/Main.groovy
+++ src/Main.groovy
@@ -14,7 +14,9 @@ class Main {
         List<Restaurant> rests = new RestaurantsParser().parse()
         def finder = new RestaurantFinder(rests)
         List<Item> searchItems = new ArrayList<>()
-        searchItems.add(new Item("fancy_european_water"))
+//        searchItems.add(new Item("jalapeno_poppers"))
+//        searchItems.add(new Item("fancy_european_water"))
+//        searchItems.add(new Item(("extreme_fajita")))
         def order = new Order(searchItems)
         SearchResult result = finder.searchRestaurant(order)
         print result
--- src/models/Order.groovy
+++ src/models/Order.groovy
@@ -1,5 +1,7 @@
 package models
 
+import com.sun.javaws.exceptions.InvalidArgumentException
+
 /**
  * Created by mukuls-webonise on 5/8/16.
  */
@@ -8,10 +10,18 @@ class Order {
 
     Order(List<Item> items) {
         this.items = items
+        checkOrderSize()
     }
 
     Order(Item item) {
         this.items = new ArrayList<>()
         items.add(item)
+        checkOrderSize()
+    }
+
+    def checkOrderSize() {
+        if (items == null || items.empty) {
+            throw new InvalidArgumentException("Order can't be empty")
+        }
     }
 }
